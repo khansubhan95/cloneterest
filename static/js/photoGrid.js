888d464538b9f5adf88651d05338d4e011ad33bc
subhan <khansubhan20@yahoo.com>
2017-04-10 21:07:10 +0530

    Add console message for credits

--- static/js/photoGrid.js
+++ static/js/photoGrid.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+	console.log('Made by @khansubhan95 https://github.com/khansubhan95');
+
 	var $grid = $('.grid').imagesLoaded( function() {
 	  $grid.masonry({
 	    itemSelector: '.grid-item',
